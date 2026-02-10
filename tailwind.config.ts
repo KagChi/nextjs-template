d73dca8274558eb66dacca65640f55ab2e3ea08e
Samuel <yonuxbot@gmail.com>
2026-02-10 14:37:51 +0000

    feat: use bun

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,21 +6,6 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      container: {
-        center: true
-      },
-      extend: {
-        objectPosition: {
-          "85-15": "85% 15%"
-        },
-        fontFamily: {
-          baloo: ["var(--font-baloo)"]
-        }
-      }
-    },
-  },
   plugins: [],
 };
 export default config;
